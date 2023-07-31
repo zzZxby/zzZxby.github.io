c3f03169281c53e3627a3509a8bd4f0c08693a26
zzZxby <2421748498@qq.com>
2023-07-31 23:08:12 +0800

    Site updated: 2023-07-31 23:08:12

---
+++ dist/music.js
@@ -0,0 +1,16 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	  {
+        name: '写不完的温柔',
+        artist: 'G.E.M.邓紫棋',
+        url: 'https://other-web-nf01-sycdn.kuwo.cn/fc80d6ca9ea83b6d388dd59e263715ac/64c7be31/resource/n3/84/54/2396262633.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/46/61/3850187071.jpg',
+      }
+     
+    ]
+});
+
+
